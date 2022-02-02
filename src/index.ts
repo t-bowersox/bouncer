a18536bcfa3b6066c865c4a18e223cadcb5cab20
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2022-02-01 21:37:02 -0500

    feat: implement createToken method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import crypto, { KeyObject } from "crypto";
+import { Base64 } from "@t-bowersox/base64";
+
+const ALGORITHM = "sha256";
+const SIGNATURE_ENCODING = "base64";
+
 export class Bouncer {
   private privateKey: KeyObject;
   private publicKey: KeyObject;
@@ -14,6 +19,37 @@ export class Bouncer {
       : crypto.createPrivateKey(privateKey);
     this.publicKey = crypto.createPublicKey(publicKey);
   }
+
+  createToken(userId: string | number, expirationDate: Date): string {
+    const sessionId = this.generateSessionId();
+    const expirationTime = expirationDate.getTime();
+    const token: Token = {
+      sessionId,
+      userId,
+      expirationTime,
+    };
+    const encodedToken = this.encodeToken(token);
+    const signature = this.signToken(encodedToken);
+    return `${encodedToken}.${signature}`;
+  }
+
+  private generateSessionId(): string {
+    return crypto.randomUUID();
+  }
+
+  private encodeToken(token: Token): Base64String {
+    const tokenJson = JSON.stringify(token);
+    return Base64.encode(tokenJson);
+  }
+
+  private signToken(encodedToken: Base64String): Base64String {
+    const signer = crypto.createSign(ALGORITHM);
+    return signer
+      .update(encodedToken)
+      .end()
+      .sign(this.privateKey, SIGNATURE_ENCODING);
+  }
+
 }
 
 export interface TokenStore {
