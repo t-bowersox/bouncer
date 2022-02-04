b461020daf87c0992de4404e7579bafeac3dc897
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2022-02-03 20:38:11 -0500

    test: add tests for evaluateSync & evaluateAsync

--- src/ruleset.test.ts
+++ src/ruleset.test.ts
@@ -5,6 +5,11 @@ describe("Ruleset", () => {
   const syncRule = (user: object) => user["role"] === "admin";
   const asyncRule = (user: object) =>
     Promise.resolve(user["role"] === "regular");
+  const user = {
+    id: 1,
+    role: "regular",
+    permissions: { read: true, write: false },
+  };
 
   beforeEach(() => {
     const initialSyncFn = () => true;
@@ -40,5 +45,43 @@ describe("Ruleset", () => {
   test("should clear async rules", () => {
     ruleset.addAsyncRule(asyncRule).clearAsyncRules();
     expect(ruleset.hasAsyncRule(asyncRule)).toBe(false);
+  });
+
+  describe("#evaluateSync", () => {
+    const syncRuleTrue = (user: object) => user["permissions"]["read"] === true;
+    const syncRuleFalse = (user: object) => user["role"] === "admin";
+
+    beforeEach(() => {
+      ruleset.clearSyncRules();
+    });
+
+    test("returns false if a sync function returns false", () => {
+      ruleset.addSyncRule(syncRuleTrue).addSyncRule(syncRuleFalse);
+      expect(ruleset.evaluateSync(user)).toBe(false);
+    });
+
+    test("returns true if all sync functions return true", () => {
+      ruleset.addSyncRule(syncRuleTrue).addSyncRule(syncRuleTrue);
+      expect(ruleset.evaluateSync(user)).toBe(true);
+    });
+  });
+
+  describe("#evaluateAsync", () => {
+    const asyncRuleTrue = async (user: object) => {
+      return Promise.resolve(user["permissions"]["read"] === true);
+    };
+    const asyncRuleFalse = async (user: object) => {
+      return Promise.resolve(user["role"] === "admin");
+    };
+
+    test("returns false if an async function returns false", async () => {
+      ruleset.addAsyncRule(asyncRuleTrue).addAsyncRule(asyncRuleFalse);
+      expect(await ruleset.evaluateAsync(user)).toBe(false);
+    });
+
+    test("returns true if all async functions return true", async () => {
+      ruleset.addAsyncRule(asyncRuleTrue).addAsyncRule(asyncRuleTrue);
+      expect(await ruleset.evaluateAsync(user)).toBe(true);
+    });
   });
 });
